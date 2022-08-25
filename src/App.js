bd23ffa77c873cb74da7dc9b6daf20d9087241f3
GuilloTT <guillemendoza023@gmail.com>
2022-08-24 23:25:19 -0300

    Primer clase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+let imagen = "kqXTUDbN";
+let titulo = "Cuarto de Libra con Queso"
+let calorias = "771 kcal"
+let descripcion = "La belleza radica en la simpleza de las cosas. Una hamburguesa que no se anda con vueltas. La perfecta combinación entre la mejor carne 100% vacuna y dos quesos que lo rodean, junto con el toque del kétchup, mostaza y la cebolla fresca."
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <div className='row'>
+      </div>
+        <div class= "col-md-4 offset-md-2">
+          <img src={imagen} alt={titulo} className="img-fluid" />
+        </div>
+
+        <div clas="col-md-4"></div>
+
+        <h1>{titulo} </h1>
+        <p>{calorias}</p>
+        <p>{descripcion}</p>
+
+
+
+    
     </div>
+    
   );
 }
 
